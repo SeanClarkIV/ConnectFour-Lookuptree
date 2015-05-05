2e1fff6da2d051de4ef3f417de0ffe790aae6cd0
Thomas Coppola <coppolt1@csmac10.lions.tcnj.edu>
2015-05-05 19:36:04 -0400

    Implemented editOutcomes

--- src/updatedfiles/src/Experience.java
+++ src/updatedfiles/src/Experience.java
@@ -29,12 +29,39 @@ public class Experience {
 		return state;
 		
 	}
-	
-	public String editOutcomes(int which, int result)
+     /*
+      * Takes a given string and modifies a specified character of that string 
+      * to a specified value.
+      * 
+      * @param which the character of the string to modify; Zero based indexing.
+      * @param result the character that replaces character at the specified index.
+      * @param outcomes the original outcomes before modification. 
+      * @return the modified string.
+      */
+    public static String editOutcomes(int which, int result, String outcomes)
 	{
-		return null;
-
-	}
-	
+	       String modifiedOutcomes="";
+	       // Bounds Checking
+	       if(which >= outcomes.length() || which < 0)
+	       {
+	    	   modifiedOutcomes = "The specifed character is outside of the string.";
+	       }
+	       else
+	       {
+	    	   modifiedOutcomes += outcomes.substring(0,which); 
+	    	   modifiedOutcomes += String.valueOf(result);
+	    	   modifiedOutcomes += outcomes.substring(which+1);
+	       }
+           return modifiedOutcomes;
+	} 
+    	public static void main(String[] args)
+    	{
+    		String Test = "10010001001001";
+    		System.out.println(Test);
+    		System.out.println(editOutcomes(1,9,Test).equals("19010001001001"));
+    		System.out.println(editOutcomes(99,0,Test).equals("The specifed character is outside of the string."));
+    		System.out.println(editOutcomes(0,9,Test).equals("90010001001001"));
+    		System.out.println(editOutcomes(13,9,Test).equals("10010001001009"));
+    }
 
 }
