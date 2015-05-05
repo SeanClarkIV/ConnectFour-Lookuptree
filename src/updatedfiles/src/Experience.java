54d5d940dccd8dc839ef6c6ff2719eec85658d01
Sean E. Clark <clarks3@tcnj.edu>
2015-05-05 17:51:44 -0400

    Added Game Experience File

---
+++ src/updatedfiles/src/Experience.java
@@ -0,0 +1,40 @@
+
+public class Experience {
+	
+	final int WIN = 2;
+	final int LOSS = 1;
+	final int UNKNOWN = 0;
+	final int DRAW = 3;
+	final int ILLEGAL = 4;
+	final int PLAYER_1 = 1;
+	final int PLAYER_2 = 2;
+	final int EMPTY = 0;
+	
+	
+	
+	
+	public void writeFile(String name, String contents)
+	{
+		
+	}
+	
+	public String readFile(String name)
+	{
+		return name;
+		
+	}
+	
+	public String generateOldBoardState(String state, int moves )
+	{
+		return state;
+		
+	}
+	
+	public String editOutcomes(int which, int result)
+	{
+		return null;
+
+	}
+	
+
+}
